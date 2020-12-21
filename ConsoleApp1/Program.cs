6d230f3fd0818d319da9e93e120a948f905f8d3e
rpkhrmn <recep.kahraman90@gmail.com>
2020-12-21 19:41:05 +0300

    aa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int a = 2;
-            int c = 3;
+            int c =4;
             Console.WriteLine(a*c);
             Console.ReadLine();
         }
